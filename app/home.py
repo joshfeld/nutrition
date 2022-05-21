319715e175e30232ed5b9e973c55aa2c67562630
kemianbd <feldmanj@gmail.com>
2022-05-21 15:59:06 -0400

    Setup

--- app/home.py
+++ app/home.py
@@ -1,8 +1,7 @@
-import functools
-
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+from werkzeug.exceptions import abort
 
 from app.auth import login_required
 
